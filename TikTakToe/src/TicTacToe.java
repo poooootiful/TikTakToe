8b99bdd3d153117b3f0cbf49dc00e24141e02d27
poooootiful <johanna_pettersson@live.com>
2023-11-30 11:27:50 +0100

    stuff

--- TikTakToe/src/TicTacToe.java
+++ TikTakToe/src/TicTacToe.java
@@ -2,47 +2,46 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.util.Random;
 
 public class TicTacToe extends JFrame implements ActionListener {
     JFrame frame = new JFrame();
     JPanel Buttons = new JPanel();
+    JPanel TurnText = new JPanel();
+    JPanel ResetPanel = new JPanel();
+     JButton Tr = new JButton("Tr");
+     JButton Tm = new JButton("Tm");
+     JButton Tl = new JButton("Tl");
+     JButton Mr = new JButton("Mr");
+     JButton Mm = new JButton("Mm");
+     JButton Ml = new JButton("Ml");
+     JButton Br = new JButton("Br");
+     JButton Bm = new JButton("Bm");
+     JButton Bl = new JButton("Bl");
 
-     JButton Tr = new JButton();
-     JButton Tm = new JButton();
-     JButton Tl = new JButton();
-     JButton Mr = new JButton();
-     JButton Mm = new JButton();
-     JButton Ml = new JButton();
-     JButton Br = new JButton();
-     JButton Bm = new JButton();
-     JButton Bl = new JButton();
-     JLabel WhoTurn = new JLabel("");
+    int Xturn = 1;
+    int Oturn =2;
 
+    int TurnCheck;
 
+    int TurnCount = 0;
+
+    public static int random (){
+        int number = 1 + (int) (2 * Math.random());
+        return number;
+    }
     TicTacToe() {
         frame.setSize(500,700);
-        JPanel Text = new JPanel();
         Buttons.setLayout(new GridLayout(3, 3));
         frame.add(Buttons);
         Buttons.setSize(500,500);
-        JPanel Panel = new JPanel();
-        frame.add(Panel);
-        Panel.setSize(500,100);
-        Text.setSize(500,100);
-        frame.add(Text);
 
+        TurnText.setSize(500,100);
+        frame.add(TurnText);
         JLabel WhoTurn= new JLabel();
-        Text.add(WhoTurn);
-        int Xturn = 1;
-        int Oturn =2;
-        int number = 1+(int) (2*Math.random());;
+        TurnText.add(WhoTurn);
 
-        if (number == Xturn) {
-            WhoTurn.setText("It's X Turn");
-        }else if (number == Oturn) {
-            WhoTurn.setText("It's O Turn");
-        }
+        frame.add(ResetPanel);
+        ResetPanel.setSize(500,100);
 
 
         Buttons.add(Tl);
@@ -62,23 +61,125 @@ public class TicTacToe extends JFrame implements ActionListener {
         Buttons.add(Bm);
         Bm.addActionListener(this);
         Buttons.add(Br);
-        Br.addActionListener(this);
-
-
-
 
 
         JButton reset = new JButton("Reset");
-        Panel.add(reset);
+        ResetPanel.add(reset);
         frame.setVisible(true);
 
+        //Who Start
+        TurnCheck = random();
+        System.out.println(TurnCheck);
 
+
+        if (TurnCheck==Xturn){
+            WhoTurn.setText("It's X Turn");
+            TurnCount = TurnCount +1;
+        }
+        if (TurnCheck==Oturn) {
+            WhoTurn.setText("It's O Turn");
+            TurnCount = TurnCount +1;
+        }
     }
-
-
-
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        String btn = e.getActionCommand();
+
+        //Tl Button
+        if (TurnCheck==Xturn && btn.matches("Tl")) {
+            Tl.doClick();
+            Tl.setText("X");
+        }
+        if (TurnCheck==Oturn && btn.matches("Tl")) {
+            Tl.doClick();
+            Tl.setText("O");
+        }
+        //Tm Button
+        if (TurnCheck==Xturn && btn.matches("Tm")) {
+            Tm.doClick();
+            Tm.setText("X");
+        }
+        if (TurnCheck==Oturn && btn.matches("Tm")) {
+            Tm.doClick();
+            Tm.setText("O");
+        }
+        //Tr Button
+        if (TurnCheck==Xturn && btn.matches("Tr")) {
+            Tr.doClick();
+            Tr.setText("X");
+            TurnCheck = Oturn;
+        }
+        if (TurnCheck==Oturn && btn.matches("Tr")) {
+            Tr.doClick();
+            Tr.setText("O");
+            TurnCheck = Xturn;
+        }
+        //Ml Button
+        if (TurnCheck==Xturn && btn.matches("Ml")) {
+            Ml.doClick();
+            Ml.setText("X");
+            TurnCheck = Oturn;
+        }
+        if (TurnCheck==Oturn && btn.matches("Ml")) {
+            Ml.doClick();
+            Ml.setText("O");
+            TurnCheck = Xturn;
+        }
+        //Mm Button
+        if (TurnCheck==Xturn && btn.matches("Mm")) {
+            Mm.doClick();
+            Mm.setText("X");
+            TurnCheck = Oturn;
+        }
+        if (TurnCheck==Oturn && btn.matches("Mm")) {
+            Mm.doClick();
+            Mm.setText("O");
+            TurnCheck = Xturn;
+        }
+        //Mr Button
+        if (TurnCheck==Xturn && btn.matches("Mr")) {
+            Mr.doClick();
+            Mr.setText("X");
+            TurnCheck = Oturn;
+        }
+        if (TurnCheck==Oturn && btn.matches("Mr")) {
+            Mr.doClick();
+            Mr.setText("O");
+            TurnCheck = Xturn;
+        }
+        //Bl Button
+        if (TurnCheck==Xturn && btn.matches("Bl")) {
+            Bl.doClick();
+            Bl.setText("X");
+            TurnCheck = Oturn;
+        }
+        if (TurnCheck==Oturn && btn.matches("Bl")) {
+            Bl.doClick();
+            Bl.setText("O");
+            TurnCheck = Xturn;
+        }
+        //Bm Button
+        if (TurnCheck==Xturn && btn.matches("Bm")) {
+            Bm.doClick();
+            Bm.setText("X");
+            TurnCheck = Oturn;
+        }
+        if (TurnCheck==Oturn && btn.matches("Bm")) {
+            Bm.doClick();
+            Bm.setText("O");
+            TurnCheck = Xturn;
+        }
+        //Br Button
+        if (TurnCheck==Xturn && btn.matches("Br")) {
+            Br.doClick();
+            Br.setText("X");
+            TurnCheck = Oturn;
+        }
+        if (TurnCheck==Oturn && btn.matches("Br")) {
+            Br.doClick();
+            Br.setText("O");
+            TurnCheck = Xturn;
+        }
     }
 }
