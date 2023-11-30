adbb31969a9387ada57dc4a889c9862d4a11cfb9
poooootiful <johanna_pettersson@live.com>
2023-11-30 13:07:46 +0100

    win conditions missing

--- TikTakToe/src/TicTacToe.java
+++ TikTakToe/src/TicTacToe.java
@@ -79,111 +79,142 @@ public class TicTacToe extends JFrame implements ActionListener {
         //Who Start
         TurnCheck = random();
 
+
         do {
-
-            if (TurnCheck == Xturn) {
-                WhoTurn.setText("It's X Turn");
-                TurnCount = TurnCount + 1;
-            }
-            if (TurnCheck == Oturn) {
-                WhoTurn.setText("It's O Turn");
-                TurnCount = TurnCount + 1;
-            }
+            System.out.println(TurnCount);
         }while (TurnCount>=9);
-        if (TurnCount==9) {
-            System.out.println("The Board is full");
 
-            if (TurnCount==9){System.out.println("It's a draw");
-            }
-        }
     }
 
     @Override
     public void actionPerformed(ActionEvent e) {
         String btn = e.getActionCommand();
 
-        if (TurnCount==6) {
-            String tl = Tl.getText().toString();
-            System.out.println(tl);
-        }
-        if (TurnCount==7) {
-
-        }
-        if (TurnCount==8) {
-
-        }
-
         //Tl Button
         if (TurnCheck==Xturn && btn.matches("Tl")) {
             Tl.setText("X");
             TurnCheck = TurnCheck+1;
+            TurnCount = TurnCount + 1;
+            System.out.println(TurnCount);
         } else if (TurnCheck==Oturn && btn.matches("Tl")) {
             Tl.setText("O");
             TurnCheck = TurnCheck -1;
+            TurnCount = TurnCount + 1;
+            System.out.println(TurnCount);
         }
         //Tm Button
         if (TurnCheck==Xturn && btn.matches("Tm")) {
             Tm.setText("X");
             TurnCheck = TurnCheck+1;
+            TurnCount = TurnCount + 1;
+            System.out.println(TurnCount);
         } else if (TurnCheck==Oturn && btn.matches("Tm")) {
             Tm.setText("O");
             TurnCheck = TurnCheck -1;
+            TurnCount = TurnCount + 1;
+            System.out.println(TurnCount);
         }
         //Tr Button
         if (TurnCheck==Xturn && btn.matches("Tr")) {
             Tr.setText("X");
             TurnCheck = TurnCheck+1;
+            TurnCount = TurnCount + 1;
+            System.out.println(TurnCount);
         } else if (TurnCheck==Oturn && btn.matches("Tr")) {
             Tr.setText("O");
             TurnCheck = TurnCheck -1;
+            TurnCount = TurnCount + 1;
+            System.out.println(TurnCount);
 
         }
         //Ml Button
         if (TurnCheck==Xturn && btn.matches("Ml")) {
             Ml.setText("X");
             TurnCheck = TurnCheck+1;
+            TurnCount = TurnCount + 1;
+            System.out.println(TurnCount);
         } else if (TurnCheck==Oturn && btn.matches("Ml")) {
             Ml.setText("O");
             TurnCheck = TurnCheck -1;
+            TurnCount = TurnCount + 1;
+            System.out.println(TurnCount);
         }
         //Mm Button
         if (TurnCheck==Xturn && btn.matches("Mm")) {
             Mm.setText("X");
             TurnCheck = TurnCheck+1;
+            TurnCount = TurnCount + 1;
+            System.out.println(TurnCount);
         } else if (TurnCheck==Oturn && btn.matches("Mm")) {
             Mm.setText("O");
+            TurnCheck=TurnCheck-1;
+            TurnCount = TurnCount + 1;
+            System.out.println(TurnCount);
         }
         //Mr Button
         if (TurnCheck==Xturn && btn.matches("Mr")) {
             Mr.setText("X");
             TurnCheck = TurnCheck+1;
+            TurnCount = TurnCount + 1;
+            System.out.println(TurnCount);
         } else if (TurnCheck==Oturn && btn.matches("Mr")) {
             Mr.setText("O");
             TurnCheck = TurnCheck -1;
+            TurnCount = TurnCount + 1;
+            System.out.println(TurnCount);
         }
         //Bl Button
         if (TurnCheck==Xturn && btn.matches("Bl")) {
             Bl.setText("X");
             TurnCheck = TurnCheck+1;
+            TurnCount = TurnCount + 1;
+            System.out.println(TurnCount);
         } else if (TurnCheck==Oturn && btn.matches("Bl")) {
             Bl.setText("O");
             TurnCheck = TurnCheck -1;
+            TurnCount = TurnCount + 1;
+            System.out.println(TurnCount);
         }
         //Bm Button
         if (TurnCheck==Xturn && btn.matches("Bm")) {
             Bm.setText("X");
             TurnCheck = TurnCheck+1;
+            TurnCount = TurnCount + 1;
+            System.out.println(TurnCount);
         } else if (TurnCheck==Oturn && btn.matches("Bm")) {
             Bm.setText("O");
             TurnCheck = TurnCheck -1;
+            TurnCount = TurnCount + 1;
+            System.out.println(TurnCount);
         }
         //Br Button
         if (TurnCheck==Xturn && btn.matches("Br")) {
             Br.setText("X");
             TurnCheck = TurnCheck+1;
+            TurnCount = TurnCount + 1;
+            System.out.println(TurnCount);
         } else if (TurnCheck==Oturn && btn.matches("Br")) {
             Br.setText("O");
             TurnCheck = TurnCheck -1;
+            TurnCount = TurnCount + 1;
+            System.out.println(TurnCount);
+        }
+        if (TurnCount==6) {
+
+        }
+
+        if (TurnCount==7) {
+
+        }
+        if (TurnCount==8) {
+
+        }
+        if (TurnCount==9) {
+            System.out.println("The Board is full");
+
+            if (TurnCount == 9) {
+                System.out.println("It's a draw");
+            }
         }
     }
 }
